a3e0c383f243fe1f2ec5a5ae0b438aa23d6603e4
Nedim Sacic <15919593+ValterBrani@users.noreply.github.com>
2025-09-29 15:27:11 -0400

    clean sql dans les Views

--- Biblioteka/Views/vw_BorrowedBooksByMember.sql
+++ Biblioteka/Views/vw_BorrowedBooksByMember.sql
@@ -1,6 +1,15 @@
 CREATE VIEW Library.vw_BorrowedBooksByMember
 AS
-SELECT m.MemberId, m.FirstName, m.LastName, b.BookId, b.Title, br.BorrowDate, br.DueDate, br.ReturnDate, br.Penalty
+SELECT 
+    m.MemberId,
+    m.FirstName,
+    m.LastName,
+    b.BookId,
+    b.Title,
+    br.BorrowDate,
+    br.DueDate,
+    br.ReturnDate,
+    br.Penalty
 FROM Library.Members m
-JOIN Library.Borrowings br ON m.MemberId = br.MemberId
-JOIN Library.Books b ON br.BookId = b.BookId;
+    JOIN Library.Borrowings br ON m.MemberId = br.MemberId
+    JOIN Library.Books b ON br.BookId = b.BookId;
